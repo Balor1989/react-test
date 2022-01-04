1a6b945e965a93cd7e621c4d14c56417cf32b59d
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-04 08:53:06 +0200

    adds refactoring ColorPicker with hooks

--- src/ColorPicker/ColorPicker.js
+++ src/ColorPicker/ColorPicker.js
@@ -5,24 +5,32 @@ import './ColorPicker.css'
 
 const ColorPicker = ({ options }) => {
     const [activeOptionIndex, setActiveOptionIndex] = useState(2);
+    const {label} = options[activeOptionIndex]
+    // const setActiveIndex = index => {
+    //     setActiveOptionIndex(index)
+    // }
 
-    const setActiveIndex = index => {
-        setActiveOptionIndex(index)
+     const addActiveClassName = (index) => {
+        const optionClasses = ['ColorPicker__option']
+                if (index === activeOptionIndex) {
+            optionClasses.push('ColorPicker__option--active')
+        }
+        return optionClasses.join(' ')
     }
 
     return (
         <div className="ColorPicker">
             <h2 className="ColorPicker__title">Color Picker</h2>
-            {/* <p>Выбран цвет: {label}</p> */}
+            <p>Выбран цвет: {label}</p>
             <div>
                 {options.map(({ label, color }, index) => {
-                    const optionClassName = this.addActiveClassName(index)
+                    const optionClassName = addActiveClassName(index)
                     return (
                         <button
                             key={label}
                             className={optionClassName}
                             style={{ backgroundColor: color }}
-                            onClick={() => this.setActiveIndex(index)}
+                            onClick={() => setActiveOptionIndex(index)}
                         ></button>
                     )
                 })}
