75a6e49c89ed0ff9324ae275dd9032754fc4c576
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-02-07 11:13:39 +0200

    save changes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Counter from './Counter/Counter';
-import Dropdown from './Dropdown/Dropdown';
-import colorPickerOptions from './ColorPicker/colorPickerOptions'
-import ColorPicker from './ColorPicker/ColorPicker';
-import App from './App'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import Counter from './Counter/Counter';
+// import Dropdown from './Dropdown/Dropdown';
+// import colorPickerOptions from './ColorPicker/colorPickerOptions'
+// import ColorPicker from './ColorPicker/ColorPicker';
+// import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <h1>Test</h1>
-    <Dropdown />
-    <p className='title'></p>
-    <Counter  />
-    <ColorPicker options={colorPickerOptions} />
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode></React.StrictMode>,
+	document.getElementById("root"),
 );
 
-
+// ReactDOM.render(
+// 	<React.StrictMode>
+// 		<h1>Test</h1>
+// 		<Dropdown />
+// 		<p className="title"></p>
+// 		<Counter />
+// 		<ColorPicker options={colorPickerOptions} />
+// 		<App />
+// 	</React.StrictMode>,
+// 	document.getElementById("root"),
+// );
 
 // ReactDOM.render(<App
 // />, document.querySelector('#root'))
@@ -30,7 +31,6 @@ ReactDOM.render(
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
-
 
 // Обычное создание
 
@@ -41,22 +41,19 @@ ReactDOM.render(
 
 // ReactDOM.render(element, document.querySelector('#root'))
 
-
 // С помощью JSX элемента
 // const name = <span>Evgeniy</span>
 // const myNameIs = <span>меня зовут</span>
 
-
 // //jsx выражение
 // const jsxElement = <h2>Привет МИР!
-//   { myNameIs}  
-//   { name}  
+//   { myNameIs}
+//   { name}
 // </h2>;
 // //jsx выражение
 
 // console.log(jsxElement)
 // ReactDOM.render(jsxElement, document.querySelector('#root'))
-
 
 // const card1 = <div>
 //   <img src={images.url} alt={images.alt} width="480" />
@@ -84,6 +81,3 @@ ReactDOM.render(
 // </div>
 
 // ReactDOM.render(card, document.querySelector('#root'))
-
-
-
