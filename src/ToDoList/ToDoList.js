aa0078947ef704254124acec974e843d9ed9cc8a
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-02-11 15:11:15 +0200

    1

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -1,19 +1,31 @@
+import { connect } from "react-redux";
+import todosActions from "../redux/todos/totos-action";
 
+const ToDoList = ({ todos, onDeleteTodo, onToggleCompleted }) => (
+	<ul className="ToDoList">
+		{todos.map(({ id, text, completed }) => (
+			<li key={id}>
+				<input
+					type="checkbox"
+					className="ToDoList__checkbox"
+					checked={completed}
+					onChange={() => onToggleCompleted(id)}
+				/>
+				<p>{completed}</p>
+				<p>{text}</p>
+				<button onClick={() => onDeleteTodo(id)}>Удалить</button>
+			</li>
+		))}
+	</ul>
+);
 
-const ToDoList = ({ todos, onDeleteTodo, onToggleCompleted}) => (
-    <ul className="ToDoList">
-        {todos.map(({ id, text, completed }) => (
-            <li key={id}>
-                <input type="checkbox"
-                    className="ToDoList__checkbox"
-                    checked={completed}
-                onChange={() => onToggleCompleted(id)}/>
-                <p>{completed}</p>
-                <p>{text}</p>
-                <button onClick={() => onDeleteTodo(id)} >Удалить</button>
-            </li>
-          ))}
-          </ul>
-)
+const mapStateToProps = (state) => ({
+	todos: state.todos.items,
+});
 
-export default ToDoList
+const mapDispatchToProps = (dispatch) => ({
+	onDeleteTodo: () => null,
+	onToggleCompleted: () => null,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ToDoList);
--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import "./index.css";
 // import colorPickerOptions from './ColorPicker/colorPickerOptions'
 // import ColorPicker from './ColorPicker/ColorPicker';
 import App from "./App";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById("root"),
 );
